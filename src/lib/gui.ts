8c489d6da54783d23bd1a94d1dd13b7b62ce5f1b
Xavier Decaudin <xd.decaudin@gmail.com>
2026-04-13 15:54:46 +0200

    chore(gui): fix VITE_ENABLE_GUI env check

--- src/lib/gui.ts
+++ src/lib/gui.ts
@@ -1,5 +1,5 @@
 import GUI from 'lil-gui';
 
-const gui = import.meta.env.VITE_ENABLE_GUI ? new GUI() : null;
+const gui = import.meta.env.VITE_ENABLE_GUI === 'true' ? new GUI() : null;
 
 export default gui;
